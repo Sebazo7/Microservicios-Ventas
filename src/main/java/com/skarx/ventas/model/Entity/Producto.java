a381d3e439593d99e2e16f74a82c24d72b59c9ee
Sebastian N. <marcelonovoanav@gmail.com>
2025-05-29 21:52:27 -0400

    UPDATE MODEL PRODUCTO

--- src/main/java/com/skarx/ventas/model/Entity/Producto.java
+++ src/main/java/com/skarx/ventas/model/Entity/Producto.java
@@ -1,5 +1,6 @@
 package com.skarx.ventas.model.Entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,12 +21,16 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String nombre;
 
+    @Column(length = 100)
     private String descripcion;
 
+    @Column(nullable = false)
     private Double precio;
 
+    @Column(nullable = false)
     private Integer stock;
 
 }
